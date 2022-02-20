8d010a6f6ff5c3d376b5b050277df2b1b527f1c3
UtrGrd <93576293+UtrGrd@users.noreply.github.com>
2022-02-20 12:14:12 +0300

    Add files via upload

---
+++ nfsc_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo su
+yum install nfs-utils
+systemctl enable firewalld --now
+systemctl status firewalld
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
+systemctl daemon-reload
+systemctl restart remote-fs.target
